8b38ee9d8bb7f600fe32f29bbee32a5d879ce8a9
R4CLucky14 <james0308@outlook.com>
2014-08-25 14:57:07 -0400

    The Entities: Setting Up The System (Part 2)

--- BetterTemplate/Models/Entities/MetaData.cs
+++ BetterTemplate/Models/Entities/MetaData.cs
@@ -1,12 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace BetterTemplate.Models.Entities
-{
-	public class MetaData
+{	
+	/// <summary>
+	/// The MetaData of an Application Entity
+	/// </summary>
+	public class MetaData : ApplicationObject
 	{
-		//todo Thunder Construct MetaData class
+		/// <summary>
+		/// The Owner of the Object
+		/// </summary>
+		public virtual ApplicationUser Owner { get; private set; }
+
+		/// <summary>
+		/// Accessor for whether the Object is Deleted
+		/// </summary>
+		public virtual Boolean Deleted { get { return this.Object.Deleted; } }
+		/// <summary>
+		/// The Object -> What the MetaData is referring to
+		/// </summary>
+		[Required]
+		public virtual ApplicationEntity Object { get; set; }
+		/// <summary>
+		/// The last User to Edit the Object
+		/// </summary>
+		public virtual ApplicationUser Editor
+		{
+			get
+			{
+				return this.Audits.OrderBy( c => c.TimeStamp ).Last().Editor;
+			}
+		}
+		/// <summary>
+		/// The last Modified TimeStamp
+		/// </summary>
+		public virtual DateTime Modified
+		{
+			get
+			{
+				return this.Audits.OrderBy( c => c.TimeStamp ).Last().TimeStamp;
+			}
+		}
+		/// <summary>
+		/// The Timestamp of the Object's creation date.
+		/// </summary>
+		public virtual DateTime Created
+		{
+			get
+			{
+				return this.Audits.OrderBy( c => c.TimeStamp ).FirstOrDefault( c => c.EventType == "Added" ).TimeStamp;
+			}
+		}
+
+
+		/// <summary>
+		/// Default Constructor for MetaData.
+		/// </summary>
+		public MetaData()
+			: base()
+		{
+			this.Audits = new Collection<Audit>();
+		}
+		/// <summary>
+		/// Constructor the Populates the Owner field
+		/// </summary>
+		/// <param name="Owner">The User who created the Object</param>
+		public MetaData( ApplicationUser Owner )
+			: this()
+		{
+			this.Owner = Owner;
+		}
 	}
 }
