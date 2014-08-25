7e580f69ca161dc4b0e35a938994cc9473802192
1BADGAMER <james0308@outlook.com>
2014-08-24 22:07:42 -0400

    Default ASP.NET MVC 5 Project

---
+++ BetterTemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BetterTemplate.MvcApplication" Language="C#" %>
---
+++ BetterTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BetterTemplate.Startup))]
+namespace BetterTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
